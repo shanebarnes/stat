5b057c30d588448e7abb669eb3af3277533e3e8c
shanebarnes <shanebarnes@users.noreply.github.com>
2021-11-10 22:49:28 -0400

    Fix Unix stat calls so that mode field reports file type

--- stat_darwin.go
+++ stat_darwin.go
@@ -14,20 +14,22 @@ func getStatInfo(name string) (*statInfo, error) {
 	abs, err := filepath.Abs(name)
 	if err == nil {
 		si.Name = abs
-		ss := syscall.Stat_t{}
-		if err = syscall.Stat(name, &ss); err == nil {
-			si.Device = uint64(ss.Dev)
-			si.Mode = (os.FileMode(ss.Mode)).String()
-			si.User = getUserName(ss.Uid)
-			si.Group = getGroupName(ss.Gid)
-			si.Size = humanize.Comma(ss.Size)
-			si.Atime = time.Unix(ss.Atimespec.Sec, ss.Atimespec.Nsec).Format(RFC3339NanoZero)
-			si.Mtime = time.Unix(ss.Mtimespec.Sec, ss.Mtimespec.Nsec).Format(RFC3339NanoZero)
-			si.Ctime = time.Unix(ss.Ctimespec.Sec, ss.Ctimespec.Nsec).Format(RFC3339NanoZero)
-			si.Btime = time.Unix(ss.Birthtimespec.Sec, ss.Birthtimespec.Nsec).Format(RFC3339NanoZero)
-			si.Blocks = humanize.Comma(ss.Blocks)
-			si.BlockSize = humanize.Comma(int64(ss.Blksize))
-			si.Flags = ss.Flags
+		var fi os.FileInfo
+		if fi, err = os.Lstat(name); err == nil {
+			if ss, ok := fi.Sys().(*syscall.Stat_t); ok {
+				si.Device = uint64(ss.Dev)
+				si.Mode = fi.Mode().String()
+				si.User = getUserName(ss.Uid)
+				si.Group = getGroupName(ss.Gid)
+				si.Size = humanize.Comma(ss.Size)
+				si.Atime = time.Unix(ss.Atimespec.Sec, ss.Atimespec.Nsec).Format(RFC3339NanoZero)
+				si.Mtime = time.Unix(ss.Mtimespec.Sec, ss.Mtimespec.Nsec).Format(RFC3339NanoZero)
+				si.Ctime = time.Unix(ss.Ctimespec.Sec, ss.Ctimespec.Nsec).Format(RFC3339NanoZero)
+				si.Btime = time.Unix(ss.Birthtimespec.Sec, ss.Birthtimespec.Nsec).Format(RFC3339NanoZero)
+				si.Blocks = humanize.Comma(ss.Blocks)
+				si.BlockSize = humanize.Comma(int64(ss.Blksize))
+				si.Flags = ss.Flags
+			}
 		}
 	}
 	return &si, err
--- stat_linux.go
+++ stat_linux.go
@@ -14,20 +14,22 @@ func getStatInfo(name string) (*statInfo, error) {
 	abs, err := filepath.Abs(name)
 	if err == nil {
 		si.Name = abs
-		ss := syscall.Stat_t{}
-		if err = syscall.Stat(name, &ss); err == nil {
-			si.Device = ss.Dev
-			si.Mode = (os.FileMode(ss.Mode)).String()
-			si.User = getUserName(ss.Uid)
-			si.Group = getGroupName(ss.Gid)
-			si.Size = humanize.Comma(ss.Size)
-			si.Atime = time.Unix(ss.Atim.Sec, ss.Atim.Nsec).Format(RFC3339NanoZero)
-			si.Mtime = time.Unix(ss.Mtim.Sec, ss.Mtim.Nsec).Format(RFC3339NanoZero)
-			si.Ctime = time.Unix(ss.Ctim.Sec, ss.Ctim.Nsec).Format(RFC3339NanoZero)
-			si.Btime = ""
-			si.Blocks = humanize.Comma(ss.Blocks)
-			si.BlockSize = humanize.Comma(int64(ss.Blksize))
-			si.Flags = uint32(0)
+		var fi os.FileInfo
+		if fi, err = os.Lstat(name); err == nil {
+			if ss, ok := fi.Sys().(*syscall.Stat_t); ok {
+				si.Device = ss.Dev
+				si.Mode = fi.Mode().String()
+				si.User = getUserName(ss.Uid)
+				si.Group = getGroupName(ss.Gid)
+				si.Size = humanize.Comma(ss.Size)
+				si.Atime = time.Unix(ss.Atim.Sec, ss.Atim.Nsec).Format(RFC3339NanoZero)
+				si.Mtime = time.Unix(ss.Mtim.Sec, ss.Mtim.Nsec).Format(RFC3339NanoZero)
+				si.Ctime = time.Unix(ss.Ctim.Sec, ss.Ctim.Nsec).Format(RFC3339NanoZero)
+				si.Btime = ""
+				si.Blocks = humanize.Comma(ss.Blocks)
+				si.BlockSize = humanize.Comma(int64(ss.Blksize))
+				si.Flags = uint32(0)
+			}
 		}
 	}
 	return &si, err
